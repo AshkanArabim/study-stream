04368fad772363e3083610eb70eb31ec243a3ae6
AshkanArabim <ashkan.arabim@gmail.com>
2024-10-12 15:16:15 -0600

    add receive_img_post view

--- backend/notes/models.py
+++ backend/notes/models.py
@@ -8,18 +8,22 @@ from django.utils import timezone
 
 class Note(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    crn = models.IntegerField(),
+    title = models.CharField(max_length=100)
+    crn = models.IntegerField()
     tally = models.IntegerField(default=0)
-    date_and_time = models.DateTimeField(default=timezone.now)
-
-    def __str__(self):
-        return f"Note by {self.user.username} on {self.date_and_time}"
+    class_date_and_time = models.DateTimeField()
 
 class TextNote(Note):
     content_text = models.TextField()
+    
+    def __str__(self):
+        return f"Text note by {self.user.username} on {self.class_date_and_time} with text '{self.content_text}'"
 
 class ImageNote(Note):
     '''
     PLEASE CHANGE THIS SO IT DOESNT CREATE FOLDER / EVENTURALLY PUSH WHEN CREATING 
     '''
     content_image = models.ImageField(upload_to='notes/images/') 
+
+    def __str__(self):
+        return f"Image note by {self.user.username} on {self.class_date_and_time} with image {self.content_image}"
--- backend/notes/views.py
+++ backend/notes/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from models import ImageNote
+from .models import ImageNote
 from django.contrib.auth.models import User 
+from datetime import datetime
+from django.utils import timezone
 
 # handle picture request
 # assumes it's guaranteed that the request is POST
@@ -12,11 +14,11 @@ def receive_img_post(request):
   # Access the JSON data
   data = request.data
   title = data.get('title')
-  body = data.get('body')
-  crn = data.get('CRN')
+  crn = data.get('crn')
+  date = data.get('date')
   
   # get the image
-  img = request.FILES.get('attachment')
+  img = request.FILES.get('img')
   
   # TODO: (waiting for login system to be done)
   fakeuser = User.objects.create_user(
@@ -27,9 +29,14 @@ def receive_img_post(request):
   # insert into db
   new_image_note = ImageNote(
     user=fakeuser,
+    title=title,
+    class_date_and_time=timezone.make_aware(
+      datetime.strptime(date, r'%Y-%m-%dT%H:%M:%S') # e.g. 2024-10-12T19:18:46
+    ),
     crn=crn,
     content_image=img
   )
+  
   new_image_note.save()
   
   # Process the data and return a response
