125cb5a5f664b724c86ddf365e877255e7ee04b4
AshkanArabim <ashkan.arabim@gmail.com>
2025-01-13 16:52:51 -0700

    recreate client vite project

--- client/src/SingleNotePage.tsx
+++
@@ -1,6 +0,0 @@
-import React, { useState } from "react";
-import Header from "./Header";
-
-const SingleNotePage = () => {
-  
-}
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,10 @@
-// main.tsx (entry point of your app)
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./App.css";
-import App from "./App";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </StrictMode>,
+)
