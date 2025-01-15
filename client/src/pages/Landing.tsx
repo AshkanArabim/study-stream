747724bc0118e9d5ee6c5e9d05b52907a0fc349d
AshkanArabim <ashkan.arabim@gmail.com>
2025-01-15 16:52:36 -0700

    change basic landing page to chakra

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import { Button, Flex, Heading, Text } from "@chakra-ui/react";
+import React from "react";
 
 const Landing: React.FC = () => {
-    return (
-        <div style={{ textAlign: 'center', padding: '50px' }}>
-            <h1>Welcome to Study Stream</h1>
-            <p>Your ultimate platform for collaborative learning.</p>
-            <button style={{ padding: '10px 20px', fontSize: '16px' }}>Get Started</button>
-        </div>
-    );
+	return (
+		<Flex direction="column" height="100vh" alignItems="stretch" width="100vw">
+			<Flex as="nav" direction="row-reverse" padding="10px" backgroundColor="red">
+				<Button variant="ghost">Log in</Button>
+			</Flex>
+			<Flex flex="1" direction="column" alignItems="center" justifyContent="center">
+				<Heading as='h1'>Study Stream!</Heading>
+				<Text>Log in to get started</Text>
+			</Flex>
+		</Flex>
+	);
 };
 
 export default Landing;
