17f6e42fa7118565259e9ca6a56f54b00ff87119
AshkanArabim <ashkan.arabim@gmail.com>
2025-01-16 21:54:58 -0700

    wire signup to backend

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,8 +1,52 @@
-import { Button, Card, Fieldset, Flex, Input, Stack } from "@chakra-ui/react";
+import { Button, Card, Fieldset, Flex, Input, Stack, Text } from "@chakra-ui/react";
 import { Field } from "@/components/ui/field";
 import { Form } from "react-router-dom";
+import { BACKEND_URL } from "@/utils/vars";
+import { extractStrings } from "@/utils/utils";
+import { useState } from "react";
 
 export default function Login() {
+	const [message, setMessage] = useState("");
+	const [failure, setFailure] = useState(false);
+
+	function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+		event.preventDefault();
+		const formData = new FormData(event.currentTarget);
+		const username = formData.get("username") as string;
+		const password = formData.get("password") as string;
+
+		// make login request
+		fetch(BACKEND_URL + "/api/auth/login", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				username: username,
+				password: password,
+			}),
+		})
+			.then(async (response) => {
+				const data = await response.json()
+
+				if (response.ok) {
+					setMessage("Login successful!");
+					setFailure(false);
+				} else {
+					const serverErrorResponse: string = extractStrings(data)[0];
+					setMessage("Login failed: " + (serverErrorResponse || "Unknown"));
+					setFailure(true);
+				}
+			})
+			.catch((error) => {
+				setMessage("Error: " + error.message);
+				setFailure(true);
+			});
+
+		// set state to loading while waiting
+		setMessage("loading...")
+	}
+	
 	return (
 		<Flex
 			alignItems="center"
@@ -13,7 +57,7 @@ export default function Login() {
 			gap="15px"
 		>
 			<Card.Root padding="10px" width="400px" maxWidth="100%">
-				<Form>
+				<Form onSubmit={handleSubmit}>
 					<Fieldset.Root>
 						<Stack>
 							<Fieldset.Legend as="h3" textStyle="2xl">
@@ -25,10 +69,10 @@ export default function Login() {
 								<Input name="username" type="text" />
 							</Field>
 							<Field label="Password" required>
-								<Input name="username" type="text" />
+								<Input name="password" type="password" />
 							</Field>
 						</Fieldset.Content>
-
+						<Text color={failure ? "fg.error" : "InfoText"}>{message}</Text>
 						<Button type="submit" mx="auto">
 							Submit
 						</Button>
